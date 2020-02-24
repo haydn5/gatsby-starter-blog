f46b7aee8a3998276fc03246c93a0541e0bee42d
Haydn Adams <haydn@beyondpapavalley.com>
2020-02-24 11:47:54 -0500

    added admin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,

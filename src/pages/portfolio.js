7e8c6b180387e601dc2ccf0ca36a3c7835376b2f
Haydn Adams <haydn@beyondpapavalley.com>
2020-02-24 14:58:54 -0500

    changed slug location

---
+++ src/pages/portfolio.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+
+const portfolio = (props) => {
+    return (
+        <span>
+            {props.data.allMarkdownRemark.edges.map(({ node }) => {
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3>
+                <Link to={node.fields.slug}>
+                  {node.frontmatter.title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+            {node.excerpt}
+            </section>
+          </article>
+        )
+      })}
+        </span>
+    )
+}
+
+export default portfolio
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
